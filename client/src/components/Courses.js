64c796d78e243a32016392a113325e35863f4079
CNLARS <53753876+CNLARS@users.noreply.github.com>
2020-08-27 18:47:55 -0700

    Semi Functional
    Adjusted Layout
    +Refactored key=id being mapped vs hard coded repetetively.

--- client/src/components/Courses.js
+++ client/src/components/Courses.js
@@ -33,7 +33,7 @@ export default class Courses extends Component{
       
         this.state.courseList.length > 0 ?
           ( courses = this.state.courseList.map( (course) => 
-          <div className="bounds">
+          <div className="bounds" key={course.id}>
             <div className="grid-33">
               <NavLink className="course--module course--link" to={`/courses/${course.id}`} >
                 <h4 className="course--label">Course {course.id}</h4>
@@ -58,16 +58,18 @@ export default class Courses extends Component{
                 <ul>
                   {courses}
                 </ul>
+              <div className="grid-33" ><a className="course--module course--add--module" href="/courses/create">
+                <h3 className="course--add--title">
+                  <svg version="1.1" xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" viewBox="0 0 13 13" className="add">
+                    <polygon points="7,6 7,0 6,0 6,6 0,6 0,7 6,7 6,13 7,13 7,7 13,7 13,6 ">
+                    </polygon>
+                  </svg>Add New Course</h3></a>
+              </div>
               </div>
             </div>
-      </div>  
+      </div> 
+       
     </div>
-
-    <div className="grid-33"><a className="course--module course--add--module" href="/courses/create">
-      <h3 className="course--add--title">
-        <svg version="1.1" xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" viewBox="0 0 13 13" className="add">
-          <polygon points="7,6 7,0 6,0 6,6 0,6 0,7 6,7 6,13 7,13 7,7 13,7 13,6 ">
-          </polygon></svg>Add New Course</h3></a></div>
   </>
           );
   }
